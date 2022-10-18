99c73ad8f13af8cb8aeba1cdcf89902d74bbf80b
Dawid Gorol <goroldawid6@gmail.com>
2022-10-18 11:13:58 +0200

    Asynchroniczne funkcje

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
 const express = require("express")
-const axios = require("axios")
 const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const fs = require('fs')
+const fsPromises = require('fs').promises;
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
+
+/*
+const coordinates = [
+  {
+    id: 1,
+    position: {
+        latitude: 49.80404,
+        longitude: 9.96233
+    },
+    timeStamp  : 125155332222
+  }
+]
+*/
+
+
+async function f(path) {
+  const data = await fsPromises.readFile(path)
+                     .catch((err) => console.error('Failed to read file', err));
+
+                     console.log( JSON.parse(data.toString()))//
+  return JSON.parse(data.toString());
+}
+
+/*function writeJson(name,data) {
+fs.writeFile(name, data, err => {
+  if (err) {
+    throw err
+  }
+  return data
+})
+}*/
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to your App!")
 })
 
-app.get("/users", (req, res) => {
-  axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(function(response) {
-      res.json(response.data)
-    }).catch(function(error) {
-      res.json("Error occured!")
+app.get("/coordinates", (req, res) => {
+
+  let data;
+
+  f('./coordinates.json').then( (response) => {
+    res.send(response)
   })
+
+  
+
 })
 
-app.post("/getUserById", (req, res) => {
-  if (!req.body.id) {
-    res.json("No ID found in reqest body.")
-  } else {
-    axios.get(`https://jsonplaceholder.typicode.com/users/${req.body.id}`)
-      .then(function(response) {
-        res.json(response.data)
-      }).catch(function(error) {
-        res.json("Error occured!")
-      })
-  }
+
+app.post("/coordinates", (req, res) => {
+  const coordinate = {
+    id: coordinates.length + 1,
+    position: req.body.position, 
+    timeStamp: req.body.timeStamp
+}
+  coordinates.push(coordinate)
+  res.send(coordinate);
 })
 
 app.listen(PORT, function () {
