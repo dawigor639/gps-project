13eb214d812c944793032311ba25ea448a90ef0b
Dawid Gorol <goroldawid6@gmail.com>
2022-11-05 10:34:10 +0100

    new POST and GET functions

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
-const fs = require('fs')
-const fsPromises = require('fs').promises;
+const fs = require("fs")
+const fsPromises = require("fs").promises;
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -12,65 +12,59 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
-/*
-const coordinates = [
-  {
-    id: 1,
-    position: {
-        latitude: 49.80404,
-        longitude: 9.96233
-    },
-    timeStamp  : 125155332222
-  }
-]
-*/
-
-
-async function f(path) {
-  const data = await fsPromises.readFile(path)
-                     .catch((err) => console.error('Failed to read file', err));
-
-                     console.log( JSON.parse(data.toString()))//
+async function fileRead(path) {
+  const data = await fsPromises.readFile(path).catch((err) => console.error("Failed reading file", err));
+  console.log( JSON.parse(data.toString()))//
   return JSON.parse(data.toString());
 }
 
-/*function writeJson(name,data) {
-fs.writeFile(name, data, err => {
-  if (err) {
-    throw err
-  }
-  return data
-})
-}*/
+async function fileWrite(path,data) {
+  fsPromises.writeFile(path,JSON.stringify(data)).catch((err) => console.error("Failed writing to file", err));
+}
 
+async function fileAppend(path,data) {
+
+  fsPromises.readFile(path).then( savedData => {
+    var json = JSON.parse(savedData);
+    json.push(data);    
+    fsPromises.writeFile(path,JSON.stringify(json)).catch((err) => console.error("Failed writing to file", err));
+  }).catch((err) => console.error("Failed reading file", err));
+
+}
 
 app.get("/", (req, res) => {
-  res.send("Welcome to your App!")
+  res.sendFile("index.html", { root: '.' })
 })
 
 app.get("/coordinates", (req, res) => {
-
-  let data;
-
-  f('./coordinates.json').then( (response) => {
-    res.send(response)
+  fileRead("./coordinates.json").then( (response) => {
+    res.status(200).send(response);
   })
+})
 
-  
+app.get("/coordinate", (req, res) => {
+  fileRead("./coordinate.json").then( (response) => {
+    res.status(200).send(response);
+  })
+})
+
+app.post("/coordinates", (req, res) => {
+  const coordinate = req.body
+  fileAppend("./coordinates.json",coordinate)
+  res.setHeader("Location", "https://gpsproject.pl/coordinates");
+  res.status(201).json({"success":"1"});
 
 })
 
+app.post("/coordinate", (req, res) => {
+  const coordinate = req.body 
+  fileWrite("./coordinate.json",coordinate) 
+  res.setHeader("Location", "https://gpsproject.pl/coordinate");
+  res.status(201).json({"success":"1"});
 
-app.post("/coordinates", (req, res) => {
-  const coordinate = {
-    id: coordinates.length + 1,
-    position: req.body.position, 
-    timeStamp: req.body.timeStamp
-}
-  coordinates.push(coordinate)
-  res.send(coordinate);
 })
 
 app.listen(PORT, function () {
-  console.log(`Express server listening on port ${PORT}`)
+
+  console.log("Express server listening on port ${PORT}")
 })
